919c69fba229beebb1bcd361d37cd6c7dee141fe
TomYOON <31751481+TomYOON@users.noreply.github.com>
2021-03-03 16:21:19 +0900

    add model Post, Counter

--- routes/posts.js
+++ routes/posts.js
@@ -4,4 +4,6 @@ const router = express.Router();
 router.get('/', (req, res) => res.render('posts/board'));
 
 router.get('/new', (req, res) => res.render('posts/new'));
+
+router.get('/test', (req, res) => res.render('posts/title'));
 module.exports = router;
