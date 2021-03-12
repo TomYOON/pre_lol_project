72e94277d1f0029fb8eaf2fa21a73901083e6b0d
s2코딩천사선영이s2 <57889360+cpplovelove@users.noreply.github.com>
2021-03-13 02:09:55 +0900

    승리팀 표시
    승리팀표시랑 에딧버튼 넣었어요! 노션도 수정했어요!

--- public/js/main.js
+++ public/js/main.js
@@ -23,30 +23,31 @@ let matchPerDay = 2;
 //onload function
 /** @return {None} */
 function processMatch(match) {
-  matches.push(mapMatchDay(match));
-  viewThisWeekMatch();
+    matches.push(mapMatchDay(match));
+    viewThisWeekMatch();
 }
 
 //build html fuction
 /** @return {String} */
 function buildVoteForm(htmlStr) {
-  return `<form method="post" action='/vote' name="voteSubmit" class="vote__form">
+    return `<form method="post" action='/vote' name="voteSubmit" class="vote__form">
   ${htmlStr}
   <input type="button" value="투표하기" class="voteSubmit__btn"/>`;
 }
 
 /** @return {String} */
 function biuldMatchHtml(match) {
-  const dateValid =
-    match.userVote != '' ||
-    dateValidCheck(match.gameStartDate, match.gameStartTime)
-      ? ''
-      : 'disabled';
+    const dateValid =
+        match.userVote != '' ||
+        dateValidCheck(match.gameStartDate, match.gameStartTime) ?
+        '' :
+        'disabled';
 
-  const htmlString = `
-  <div class="matching">
 
-  <label class="left__team ${match.userVote == 'home' ? 'voted' : ''}">
+
+    const htmlString = `
+  <div class="matching">    
+  <label class="left__team ${match.userVote == 'home' ? 'voted' : ''}"  style="background-color:${match.homeTeamVotes>match.awayTeamVotes? '#2c4cfb' : '#6395d1'}">
   <input type="radio" name=${
     match._id
   } id="vote__input" value='home' class="match__ladioBtn" ${dateValid}/>
@@ -54,8 +55,10 @@ function biuldMatchHtml(match) {
     <span class="team__name">${match.homeTeamName}</span>
     <span class="votes">${match.homeTeamVotes}표</span>
   </label>
+
   <span class="vs">VS</span>
-  <label class="right__team ${match.userVote == 'away' ? 'voted' : ''}">
+
+  <label class="right__team ${match.userVote == 'away' ? 'voted' : ''}" style="background-color:${match.homeTeamVotes<match.awayTeamVotes? '#2c4cfb' : '#6395d1'}">
     <span class="votes">${match.awayTeamVotes}표</span>
     <span class="team__name">${match.awayTeamName}</span>
     <img src="/images/${match.awayTeamCode}.png" alt="team logo" />
@@ -64,225 +67,227 @@ function biuldMatchHtml(match) {
     } id="vote__input" value='away' class="match__ladioBtn" ${dateValid}/>
   </label></div>
   `;
-  return htmlString;
+
+
+    return htmlString;
 }
 
 // fetch function
 /** @return {Array<object>} */
 async function fetchMatch(date, next = true) {
-  // console.log(date);
-  const data = await fetch(
-    `/match?date=${formatDate(date)}&next=${next}`
-  ).then((res) => res.json());
-  return data;
+    // console.log(date);
+    const data = await fetch(
+        `/match?date=${formatDate(date)}&next=${next}`
+    ).then((res) => res.json());
+    return data;
 }
 
 /** @return {Array<object>} */
 async function fetchVote(votes) {
-  // console.log(votes);
-  let data = await fetch('/vote', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(votes),
-  }).then((res) => res.json());
-  return data;
+    // console.log(votes);
+    let data = await fetch('/vote', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(votes),
+    }).then((res) => res.json());
+    return data;
 }
 
 // process match function
 /** @return {Array<object>} */
 function mapMatchDay(matches) {
-  if (matches.length < 1) return;
+    if (matches.length < 1) return;
 
-  /** match를 시간순으로 정렬 */
-  matches.sort((a, b) =>
-    a.gameStartDate > b.gameStartDate
-      ? 1
-      : a.gameStartDate === b.gameStartDate
-      ? a.gameStartTime > b.gameStartTime
-        ? 1
-        : -1
-      : -1
-  );
+    /** match를 시간순으로 정렬 */
+    matches.sort((a, b) =>
+        a.gameStartDate > b.gameStartDate ?
+        1 :
+        a.gameStartDate === b.gameStartDate ?
+        a.gameStartTime > b.gameStartTime ?
+        1 :
+        -1 :
+        -1
+    );
 
-  let curDate = matches[0].gameStartDate;
-  let mapedArr = [];
-  let dateArr = []; //날짜가 같은 경기를 담을 배열
+    let curDate = matches[0].gameStartDate;
+    let mapedArr = [];
+    let dateArr = []; //날짜가 같은 경기를 담을 배열
 
-  for (let i = 0; i < matches.length; i++) {
-    if (matches[i].gameStartDate == curDate) {
-      dateArr.push(matches[i]);
-    } else {
-      curDate = matches[i].gameStartDate;
-      mapedArr.push(dateArr);
-      dateArr = [matches[i]];
+    for (let i = 0; i < matches.length; i++) {
+        if (matches[i].gameStartDate == curDate) {
+            dateArr.push(matches[i]);
+        } else {
+            curDate = matches[i].gameStartDate;
+            mapedArr.push(dateArr);
+            dateArr = [matches[i]];
+        }
     }
-  }
-  mapedArr.push(dateArr);
-  return mapedArr;
+    mapedArr.push(dateArr);
+    return mapedArr;
 }
 
 /** @return {bool} */
 function dateValidCheck(matchDate, matchTime) {
-  const today = new Date();
-  const date = formatDate(today);
-  const curHour = today.getHours();
-  let isOverTime = true;
+    const today = new Date();
+    const date = formatDate(today);
+    const curHour = today.getHours();
+    let isOverTime = true;
 
-  if (matchDate < date) {
-    isOverTime = false;
-  } else if (matchDate == date) {
-    if (parseInt(matchTime.substring(0, 2)) <= curHour) {
-      isOverTime = false;
+    if (matchDate < date) {
+        isOverTime = false;
+    } else if (matchDate == date) {
+        if (parseInt(matchTime.substring(0, 2)) <= curHour) {
+            isOverTime = false;
+        }
     }
-  }
 
-  return isOverTime;
+    return isOverTime;
 }
 
 /** @return {None} */
 function getDay(dateStr) {
-  const date = new Date(dateStr);
-  const day = date.getDay();
-  const dayArr = ['일', '월', '화', '수', '목', '금', '토'];
-  return dayArr[day];
+    const date = new Date(dateStr);
+    const day = date.getDay();
+    const dayArr = ['일', '월', '화', '수', '목', '금', '토'];
+    return dayArr[day];
 }
 
 /** @return {None} */
 function viewThisWeekMatch() {
-  container.innerHTML = '';
+    container.innerHTML = '';
 
-  const curMatches = matches[curIdx];
-  for (const dayMappedMatch of curMatches) {
-    const matchDiv = document.createElement('div');
-    const dateSpan = document.createElement('span');
+    const curMatches = matches[curIdx];
+    for (const dayMappedMatch of curMatches) {
+        const matchDiv = document.createElement('div');
+        const dateSpan = document.createElement('span');
 
-    matchDiv.classList.add('day__match');
-    dateSpan.classList.add('dateSpan');
+        matchDiv.classList.add('day__match');
+        dateSpan.classList.add('dateSpan');
 
-    dateSpan.innerHTML = `${dayMappedMatch[0].gameStartDate} (${getDay(
+        dateSpan.innerHTML = `${dayMappedMatch[0].gameStartDate} (${getDay(
       dayMappedMatch[0].gameStartDate
     )})`;
-    matchDiv.appendChild(dateSpan);
+        matchDiv.appendChild(dateSpan);
 
-    for (const match of dayMappedMatch) {
-      const matchLi = document.createElement('li');
-      matchLi.classList.add('match__list');
-      if (
-        match.userVote != '' ||
-        !dateValidCheck(match.gameStartDate, match.gameStartTime)
-      ) {
-        matchLi.classList.add('ended__match');
-      }
+        for (const match of dayMappedMatch) {
+            const matchLi = document.createElement('li');
+            matchLi.classList.add('match__list');
+            if (
+                match.userVote != '' ||
+                !dateValidCheck(match.gameStartDate, match.gameStartTime)
+            ) {
+                matchLi.classList.add('ended__match');
+            }
 
-      const htmlString = biuldMatchHtml(match);
-      matchLi.innerHTML = htmlString;
-      matchDiv.appendChild(matchLi);
+            const htmlString = biuldMatchHtml(match);
+            matchLi.innerHTML = htmlString;
+            matchDiv.appendChild(matchLi);
+        }
+        container.appendChild(matchDiv);
     }
-    container.appendChild(matchDiv);
-  }
 }
 
 /** @return {String} */
 function formatDate(date) {
-  if (typeof date == typeof new Date()) {
-    return date.toISOString().substring(0, 10);
-  }
-  return date.substring(0, 10);
+    if (typeof date == typeof new Date()) {
+        return date.toISOString().substring(0, 10);
+    }
+    return date.substring(0, 10);
 }
 
 // button listener function
 /** @return {None} */
 async function getNextMatch() {
-  leftBtn.disabled = false;
-  alertSpan.innerHTML = '';
-  // console.log(matches[curIdx][0][0].gameStartDate);
-  if (curIdx === matches.length - 1) {
-    const curDate = new Date(matches[curIdx][0][0].gameStartDate);
-    curDate.setDate(curDate.getDate() + 7);
-    console.log(123, curDate);
-    const matchArr = await fetchMatch(curDate);
-    if (matchArr.length > 0) {
-      matches.push(mapMatchDay(matchArr));
-      curIdx++;
+    leftBtn.disabled = false;
+    alertSpan.innerHTML = '';
+    // console.log(matches[curIdx][0][0].gameStartDate);
+    if (curIdx === matches.length - 1) {
+        const curDate = new Date(matches[curIdx][0][0].gameStartDate);
+        curDate.setDate(curDate.getDate() + 7);
+        console.log(123, curDate);
+        const matchArr = await fetchMatch(curDate);
+        if (matchArr.length > 0) {
+            matches.push(mapMatchDay(matchArr));
+            curIdx++;
+        } else {
+            alertSpan.innerHTML = '경기가 없습니다.';
+            rightBtn.disabled = true;
+        }
     } else {
-      alertSpan.innerHTML = '경기가 없습니다.';
-      rightBtn.disabled = true;
+        curIdx++;
     }
-  } else {
-    curIdx++;
-  }
-  viewThisWeekMatch();
+    viewThisWeekMatch();
 }
 
 /** @return {None}*/
 async function getPrevMatch() {
-  alertSpan.innerHTML = '';
-  rightBtn.disabled = false;
-  // console.log(matches[curIdx][0][0].gameStartDate);
-  if (curIdx === 0) {
-    const curDate = new Date(matches[curIdx][0][0].gameStartDate);
-    curDate.setDate(curDate.getDate() - 7);
-    const matchArr = await fetchMatch(curDate, (next = false));
-    if (matchArr.length > 0) {
-      // matches.push(mapMatchDay(matchArr));
-      matches.splice(0, 0, mapMatchDay(matchArr));
+    alertSpan.innerHTML = '';
+    rightBtn.disabled = false;
+    // console.log(matches[curIdx][0][0].gameStartDate);
+    if (curIdx === 0) {
+        const curDate = new Date(matches[curIdx][0][0].gameStartDate);
+        curDate.setDate(curDate.getDate() - 7);
+        const matchArr = await fetchMatch(curDate, (next = false));
+        if (matchArr.length > 0) {
+            // matches.push(mapMatchDay(matchArr));
+            matches.splice(0, 0, mapMatchDay(matchArr));
+        } else {
+            alertSpan.innerHTML = '경기가 없습니다.';
+            leftBtn.disabled = true;
+        }
     } else {
-      alertSpan.innerHTML = '경기가 없습니다.';
-      leftBtn.disabled = true;
+        curIdx--;
     }
-  } else {
-    curIdx--;
-  }
-  viewThisWeekMatch();
+    viewThisWeekMatch();
 }
 
 /** @return {None} */
 async function submitVote(event) {
-  event.preventDefault();
-  const votes = {};
-  const form = document.voteSubmit;
-  for (const dayMatch of matches[curIdx]) {
-    for (const match of dayMatch) {
-      let elements = document.getElementsByName(match._id);
-      if (elements[0].checked) {
-        votes[match._id] = elements[0].value;
-      } else if (elements[1].checked) {
-        votes[match._id] = elements[1].value;
-      }
-    }
-  }
-  if (votes) {
-    const data = await fetchVote(votes);
-    // console.log(data);
-    if (data.status == 'login') {
-      alert('로그인이 필요합니다.');
-    } else if (data.status == 'OK') {
-      const votedArr = data.voted;
-
-      for (const dayMatch of matches[curIdx]) {
+    event.preventDefault();
+    const votes = {};
+    const form = document.voteSubmit;
+    for (const dayMatch of matches[curIdx]) {
         for (const match of dayMatch) {
-          if (checkVoteId(match._id, votedArr)) {
-            const votedTeam = votes[match._id];
-            match.userVote = votedTeam;
-            match[`${votedTeam}TeamVotes`]++;
-          }
+            let elements = document.getElementsByName(match._id);
+            if (elements[0].checked) {
+                votes[match._id] = elements[0].value;
+            } else if (elements[1].checked) {
+                votes[match._id] = elements[1].value;
+            }
         }
-      }
-      viewThisWeekMatch();
     }
-  } else {
-    alert('한 개 이상 투표하셔야 합니다!');
-  }
+    if (votes) {
+        const data = await fetchVote(votes);
+        // console.log(data);
+        if (data.status == 'login') {
+            alert('로그인이 필요합니다.');
+        } else if (data.status == 'OK') {
+            const votedArr = data.voted;
+
+            for (const dayMatch of matches[curIdx]) {
+                for (const match of dayMatch) {
+                    if (checkVoteId(match._id, votedArr)) {
+                        const votedTeam = votes[match._id];
+                        match.userVote = votedTeam;
+                        match[`${votedTeam}TeamVotes`]++;
+                    }
+                }
+            }
+            viewThisWeekMatch();
+        }
+    } else {
+        alert('한 개 이상 투표하셔야 합니다!');
+    }
 }
 
 /** @return {Bool} */
 function checkVoteId(id, votes) {
-  for (const vote of votes) {
-    if (id == vote) {
-      return true;
+    for (const vote of votes) {
+        if (id == vote) {
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
